e7740f07fa04d4bb2eb01e5ccc98888da6236493
ycw790710 <ycw790710@gmail.com>
2023-07-10 13:11:16 +0800

    新增task測試

--- TestGrpcClientConsoleApp/Program.cs
+++ TestGrpcClientConsoleApp/Program.cs
@@ -8,27 +8,114 @@ namespace TestGrpcClientConsoleApp
     {
         static async Task Main(string[] args)
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:52345/");
+            var uriAddress = "https://localhost:52345/";
+
+            using GrpcChannel channel = GrpcChannel.ForAddress(uriAddress);
             await channel.ConnectAsync();
-            var client = new Greeter.GreeterClient(channel);
             Console.WriteLine($"{channel.State}");
 
+            await TestIteration(channel);
+            TestTasks(channel);
+
+            Console.WriteLine($"{channel.State}");
+            Console.ReadLine();
+        }
+
+        static async Task TestIteration(GrpcChannel channel)
+        {
+            Console.WriteLine($"---{nameof(TestIteration)}---");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            Stopwatch sw = new();
             int count = 0;
             int targetMilliseconds = 3000;
-            Stopwatch sw = new();
+
             sw.Start();
             while (sw.ElapsedMilliseconds <= targetMilliseconds)
             {
                 var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
                 var msg = reply.Message;
                 Interlocked.Increment(ref count);
-            }            
+                Console.Write($"\rcount：{count}");
+            }
+            Console.WriteLine();
             sw.Stop();
-            Console.WriteLine($"ElapsedMilliseconds:{sw.ElapsedMilliseconds}");
-            Console.WriteLine($"count:{count}");
-            Console.WriteLine($"{channel.State}");
 
-            Console.ReadLine();
+            Console.WriteLine($"Elapsed Milliseconds:{sw.ElapsedMilliseconds}");
+            Console.WriteLine($"count:{count}");
+            Console.WriteLine();
+        }
+
+        static void TestTasks(GrpcChannel channel)
+        {
+            Console.WriteLine($"---{nameof(TestTasks)}---");
+
+            Stopwatch sw = new();
+            int count = 0;
+            int targetMilliseconds = 3000;
+            ManualResetEventSlim manualResetEventSlim = new(false);
+            int taskCount = 40;
+            int readyCount = 0;
+            int finishedCount = 0;
+
+            object _lockCreateClientMilliseconds = new();
+            long createClientTicks = 0;
+
+            for (int i = 0; i < taskCount; i++)
+            {
+                Task.Run(async () =>
+                {
+                    var a = i;
+                    var b = 0;
+
+                    Stopwatch swClient = new();
+                    swClient.Start();
+                    var client = new Greeter.GreeterClient(channel);
+                    swClient.Stop();
+                    lock(_lockCreateClientMilliseconds)
+                        createClientTicks += swClient.ElapsedTicks;
+
+                    Interlocked.Increment(ref readyCount);
+                    manualResetEventSlim.Wait();
+                    while (sw.ElapsedMilliseconds <= targetMilliseconds)
+                    {
+                        var id = $"{a},{b}";
+                        var reply = await client.SayHelloAsync(new HelloRequest { Name = $"Msg,{id}" });
+                        var msg = reply.Message;
+                        if (!msg.Contains(id))
+                            break;
+                        Interlocked.Increment(ref count);
+                        b++;
+                    }
+                    Interlocked.Increment(ref finishedCount);
+                });
+            }
+
+            Console.WriteLine($"tasks count:{taskCount}");
+            while (readyCount < taskCount)
+            {
+                var pre = readyCount;
+                SpinWait.SpinUntil(() => pre != readyCount || readyCount == taskCount);
+                Console.Write($"\r準備task進度：{readyCount}/{taskCount}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"create client ticks:{createClientTicks}");
+
+            sw.Start();
+            manualResetEventSlim.Set();
+            while (finishedCount < taskCount)
+            {
+                var pre = finishedCount;
+                SpinWait.SpinUntil(() => pre != finishedCount || finishedCount == taskCount);
+                Console.Write($"\r完成task進度：{finishedCount}/{taskCount}");
+            }
+            Console.WriteLine();
+            sw.Stop();
+
+            Console.WriteLine($"Elapsed Milliseconds:{sw.ElapsedMilliseconds}");
+            Console.WriteLine($"count:{count}");
+            Console.WriteLine();
         }
     }
 }
